b3e9ec8a6552902f7f0bef3fb66b5b0f182ee4c1
Mario Marinero <mariomarinero@gmail.com>
2014-11-03 00:33:20 +0100

    Base html, css (empty) and js files with example code to configure JQuery and QUnit with requirejs

---
+++ main.js
@@ -0,0 +1,22 @@
+require.config({
+    paths: {
+        'QUnit': 'http://code.jquery.com/qunit/qunit-1.15.0',
+		'jquery': 'https://code.jquery.com/jquery-2.1.1'
+    },
+    shim: {
+       'QUnit': {
+           exports: 'QUnit',
+           init: function() {
+               QUnit.config.autoload = false;
+               QUnit.config.autostart = false;
+           }
+       } 
+    }
+});
+
+define(['jquery', 'QUnit'], function(jQuery, QUnit){
+
+
+QUnit.load();
+QUnit.start();
+});
